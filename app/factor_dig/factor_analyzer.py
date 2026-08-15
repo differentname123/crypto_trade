019f665ffa4ab3265e6407e3a2801163d8fc8c13
differentname123 <2434954452@qq.com>
2026-08-15 22:34:31 +0800

    2026-08-15:     已完成:         1.因子挖掘优化：1.增加全为true的信号         2.自动交易：1.，本地上线ma_bottom_long 策略
        待完成：
            -13.将strategy_ma_bottom_long.py 上线本地交易
            -12.继续优化山寨币的 做多策略 factor_dig.py 进行多因子挖掘 factor_analyzer.py进行整体的分析.要删除非close 购买的因子. 并且引入 截面的top_k 方便进行确切的统计
            -11.尽量今天就跑通整个分析类帖子创作流程
            -10.重新推到重来,严格的实现高质量输出结果,所以前面一一文章分析提取的时候就非常重要( 重新写目标 内容生成方案_分析类.txt),
            -9.生成内容还需要的步骤:1.组织不同的输入数据 当前标签非常丰富,感觉都不需要进行向量化存储,都能够通过标签筛选. 2.生成不同的提示词 3.新的mongodb表保存
            -8.进行一步一步的内容生成 格式化，先进行结构化，然后进行向量化，最后进行主题创作. 开始进行最终的两种类型文章的生成,上下文在 temp5.json里面
            -7.生成推广评论增加图片转的文字信息,然后要保证超链接文本没有 $ 这些特殊字符
            -6.推广的评论也要有水军,以及当前有效效果很差,暗示不足的话还不如不发.做like 和收藏 也要进行统计和准确的记录数据
            -2.完善基础的数据拉取 发帖 和 回复 这种基础的功能 （当前只能纯文字 尝试使用playwright模拟真实的发帖）
            -1.规划自动发帖的方案，目标是引流 增加信任，反正也是一个核心，以目的为起始（合适的文本转图片，方便降低理解门槛）
            -0.75.进行网址申请adsence，现有的内容不足以支撑，要等到后续能够参数内容贴子的时候再增加到网页内容上，以便于通过审核，它们喜欢内容网站而不是工具网站
            -0.5.关于预测市场的策略回测与策略尝试
            0.75.实现一个通用的反馈的按钮已经相应的接口，供所有的页面进行使用
            1.自动内容的生成搬运
            3.更多的赚钱方式（ai生成内容 + 内容平台分发（youtube 国内平台 去中心化平台））

--- app/factor_dig/factor_analyzer.py
+++ app/factor_dig/factor_analyzer.py
@@ -103,10 +103,10 @@ def print_row(label, vals):
 # ==========================================
 def load_and_prep_data():
     paths = {
-        '60m': './summary_results_60m/aggregated_summary_Long.csv',
-        '30m': './summary_results_30m/aggregated_summary_Long.csv',
-        '15m': './summary_results_15m/aggregated_summary_Long.csv',
-        '5m': './summary_results_5m/aggregated_summary_Long.csv'
+        '60m': './summary_results_60m/aggregated_summary_Short.csv',
+        '30m': './summary_results_30m/aggregated_summary_Short.csv',
+        '15m': './summary_results_15m/aggregated_summary_Short.csv',
+        '5m': './summary_results_5m/aggregated_summary_Short.csv'
     }
 
     dfs = {}
@@ -426,10 +426,10 @@ def query_strategy_combination(entry_name, exit_name, filter_name):
     # 1. 动态读取并精准过滤数据
     # ==========================================
     paths = {
-        '60m': './summary_results_60m/aggregated_summary_Long.csv',
-        '30m': './summary_results_30m/aggregated_summary_Long.csv',
-        '15m': './summary_results_15m/aggregated_summary_Long.csv',
-        '5m': './summary_results_5m/aggregated_summary_Long.csv'
+        '60m': './summary_results_60m/aggregated_summary_Short.csv',
+        '30m': './summary_results_30m/aggregated_summary_Short.csv',
+        '15m': './summary_results_15m/aggregated_summary_Short.csv',
+        '5m': './summary_results_5m/aggregated_summary_Short.csv'
     }
 
     dfs = []
